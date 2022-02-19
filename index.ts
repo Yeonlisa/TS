d71a3df7eed51be52ec86fdd80979ae5b2e6529a
yeonlisa <tkzw4869@gmail.com>
2022-02-19 20:21:03 +0900

    타입스크립트 기본 타입 정리 (primitive types)

--- index.ts
+++ index.ts
@@ -1,22 +1,5 @@
-// 타입: string, number, boolean, null, undefined, bigint, [], {} 등
-// string[] : array안에 string만 들어올 수 있다
-// { name? : string } = { name: 'kim' } :  name 속성은 옵션이다
-type 내타입 = string | number // Type alias
-
-let 이름 : 내타입 = 'kim'; // Union type
-
-function 함수(x : number) : number{
-    return x * 2
-} // 이 함수는 파라미터로 number, return 값으로 number
-
-type Member = {
-    [key : string] : string,
-}
-let john : Member = { name: 'kim' }
-
-class User {
-    name : string;
-    constructor(name : string) {
-        this.name = name;
-    }
-}
+let 이름 : string = 'kim';
+let 나이 : number = 50;
+let 결혼했니 : boolean = false;
+let 회원들 : string[] = ['kim', 'park']
+let 내정보 : { age : number } = { age : 20 };
